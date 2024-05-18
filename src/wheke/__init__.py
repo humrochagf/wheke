23cecd00bf0e3733ef8458c117861b02d3c6c987
Humberto Rocha <humrochagf@gmail.com>
2024-05-17 22:11:02 -0400

    Expose ServiceList

--- src/wheke/__init__.py
+++ src/wheke/__init__.py
@@ -1,11 +1,12 @@
 from ._core import Wheke
 from ._demo import demo_pod
-from ._pod import Pod
+from ._pod import Pod, ServiceList
 from ._service import aget_service, get_service
 from ._settings import settings
 
 __all__ = [
     "Pod",
+    "ServiceList",
     "Wheke",
     "aget_service",
     "demo_pod",
